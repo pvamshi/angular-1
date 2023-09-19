rajis7474/angular

fix(compiler): do not use constants for colocated shallow references
Currently the TS generated output (not JIT), breaks at runtime because
we emit all variables using the `Final` modifier. i.e. constants.

This breaks shallow references when we run our acceptance signal
tests using AOT.

packages/compiler/src/template/pipeline/src/phases/generate_variables.ts
@@ -8,7 +8,6 @@
 
 import * as o from '../../../../output/output_ast';
 import * as ir from '../../ir';
-
 import type {ComponentCompilation, ViewCompilation} from '../compilation';
 
 /**
@@ -180,20 +179,22 @@ function generateVariablesInScopeForView<OpT extends ir.Op<OpT>>(
     // view with a `nextContext` expression. This context switching operation itself declares a
     // variable, because the context of the view may be referenced directly.
     newOps.push(ir.createVariableOp(
-        view.tpl.allocateXrefId(), scope.viewContextVariable, new ir.NextContextExpr()));
+        view.tpl.allocateXrefId(), scope.viewContextVariable, new ir.NextContextExpr(),
+        /* isConstant */ true));
   }
 
   // Add variables for all context variables available in this scope's view.
   for (const [name, value] of view.tpl.views.get(scope.view)!.contextVariables) {
     newOps.push(ir.createVariableOp(
         view.tpl.allocateXrefId(), scope.contextVariables.get(name)!,
-        new o.ReadPropExpr(new ir.ContextExpr(scope.view), value)));
+        new o.ReadPropExpr(new ir.ContextExpr(scope.view), value), /* isConstant */ true));
   }
 
   // Add variables for all local references declared for elements in this scope.
   for (const ref of scope.references) {
     newOps.push(ir.createVariableOp(
-        view.tpl.allocateXrefId(), ref.variable, new ir.ReferenceExpr(ref.targetId, ref.offset)));
+        view.tpl.allocateXrefId(), ref.variable, new ir.ReferenceExpr(ref.targetId, ref.offset),
+        /* isConstant */ true));
   }
 
   if (scope.parent !== null) {

packages/compiler/src/template/pipeline/src/phases/save_restore_view.ts
@@ -19,7 +19,8 @@ export function phaseSaveRestoreView(cpl: ComponentCompilation): void {
             name: null,
             view: view.xref,
           },
-          new ir.GetCurrentViewExpr()),
+          new ir.GetCurrentViewExpr(), /* isConstant */ true),
+
     ]);
 
     for (const op of view.create) {
@@ -56,7 +57,7 @@ function addSaveRestoreViewOperationToListener(view: ViewCompilation, op: ir.Lis
           name: null,
           view: view.xref,
         },
-        new ir.RestoreViewExpr(view.xref)),
+        new ir.RestoreViewExpr(view.xref), /* isConstant */ true),
   ]);
 
   // The "restore view" operation in listeners requires a call to `resetView` to reset the

pvamshi/angular-1

packages/compiler/src/template/pipeline/src/phases/generate_variables.ts
@@ -179,20 +179,22 @@ function generateVariablesInScopeForView<OpT extends ir.Op<OpT>>(
     // view with a `nextContext` expression. This context switching operation itself declares a
     // variable, because the context of the view may be referenced directly.
     newOps.push(ir.createVariableOp(
-        view.job.allocateXrefId(), scope.viewContextVariable, new ir.NextContextExpr()));
+        view.job.allocateXrefId(), scope.viewContextVariable, new ir.NextContextExpr(),
+        /* isConstant */ true));
   }
 
   // Add variables for all context variables available in this scope's view.
   for (const [name, value] of view.job.views.get(scope.view)!.contextVariables) {
     newOps.push(ir.createVariableOp(
         view.job.allocateXrefId(), scope.contextVariables.get(name)!,
-        new o.ReadPropExpr(new ir.ContextExpr(scope.view), value)));
+        new o.ReadPropExpr(new ir.ContextExpr(scope.view), value), /* isConstant */ true));
   }
 
   // Add variables for all local references declared for elements in this scope.
   for (const ref of scope.references) {
     newOps.push(ir.createVariableOp(
-        view.job.allocateXrefId(), ref.variable, new ir.ReferenceExpr(ref.targetId, ref.offset)));
+        view.job.allocateXrefId(), ref.variable, new ir.ReferenceExpr(ref.targetId, ref.offset),
+        /* isConstant */ true));
   }
 
   if (scope.parent !== null) {

packages/compiler/src/template/pipeline/src/phases/save_restore_view.ts
@@ -19,7 +19,8 @@ export function phaseSaveRestoreView(job: ComponentCompilationJob): void {
             name: null,
             view: view.xref,
           },
-          new ir.GetCurrentViewExpr()),
+          new ir.GetCurrentViewExpr(), /* isConstant */ true),
+
     ]);
 
     for (const op of view.create) {
@@ -56,7 +57,7 @@ function addSaveRestoreViewOperationToListener(unit: ViewCompilationUnit, op: ir
           name: null,
           view: unit.xref,
         },
-        new ir.RestoreViewExpr(unit.xref)),
+        new ir.RestoreViewExpr(unit.xref), /* isConstant */ true),
   ]);
 
   // The "restore view" operation in listeners requires a call to `resetView` to reset the